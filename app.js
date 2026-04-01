d0188b4137c19c3699a621fed30cb6f78829ff83
Preethi P Palankar <preetippalankar@gmail.com>
2026-04-01 23:28:21 +0530

    backend connected to mongodb successfully

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send("API Running...");
+  res.send("API Running 🚀");
 });
 
 module.exports = app;
